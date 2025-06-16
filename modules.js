8e632c2079183b5ed093065e1e537a790f3bb389
Naruto632 <NarutoKeed@gmail.com>
2025-06-16 10:19:11 +0200

    Create modules.js

---
+++ modules.js
@@ -0,0 +1,10 @@
+// Define a function
+function addNumbers(a, b) {
+  return a + b;
+}
+
+// Define a variable
+const PI = 3.14;
+
+// Export the function and variable
+module.exports = { addNumbers, PI };
