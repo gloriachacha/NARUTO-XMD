4c3912aa26efcbf07c29ace24d73692b5d216d3f
Naruto632 <NarutoKeed@gmail.com>
2025-06-20 02:55:28 +0200

    Delete NARUTO-XMD-BOT directory

--- NARUTO-XMD-BOT/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  apiKey: 'SK3d0c21de33ee2704d1a02d38f053ca1a',
-  apiSecret: '7d4ae0a8b09c9f8f59dc247ba1a2705c',
-  // Add other configuration settings as needed
-};
-
-module.exports = config;
